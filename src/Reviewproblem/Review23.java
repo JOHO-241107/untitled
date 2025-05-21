382ee8f273b4635eff7b1b9875cd2df958dc4dec
HoriMasataka <241107@std.hi-joho.ac.jp>
2025-05-21 10:42:35 +0900

    GitHubにアップロードする手順を確認するためのデモ作業

---
+++ src/Reviewproblem/Review23.java
@@ -0,0 +1,12 @@
+package Reviewproblem;
+
+public class Review23 {
+    public static void main(String[] args) {
+        int num = 5;
+        int sum = 1;
+        for (int i = 1; i <= num; i++) {
+            sum *= i;
+        }
+        System.out.println("結果:" + sum);
+    }
+}
