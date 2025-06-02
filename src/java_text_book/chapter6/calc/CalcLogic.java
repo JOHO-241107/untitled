ae45c5d3432f50cb0881e0886205e3ba72d4ed4f
HoriMasataka <241107@std.hi-joho.ac.jp>
2025-06-02 12:29:54 +0900

    教科書問題

--- src/java_text_book/chapter6/calc/CalcLogic.java
+++ src/java_text_book/chapter6/calc/CalcLogic.java
@@ -1,8 +1,8 @@
 package java_text_book.chapter6.calc;
 
 public class CalcLogic {
-    public static int tasu(int a, int c){
-    return (a+c);
+    public static int tasu(int a, int b){
+    return (a+b);
     }
     public static int hiku(int a, int b){
         return (a-b);
